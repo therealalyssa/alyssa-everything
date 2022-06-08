a9684cff8968ab4787472ad5d8cbbb45db07ae88
Alyssa Barrett <cicibarrett@gmail.com>
2022-06-08 12:50:51 +1200

    mission 2

--- website2/app.js
+++ website2/app.js
@@ -1,114 +1,109 @@
-
 // Signup forms
 
 let person = prompt("Please enter your name", "Harry Potter");
 let text;
 if (person == null || person == "") {
-  text = "User cancelled the prompt.";
+  alert("User cancelled the prompt.");
 } else {
-  text = "Hello " + person + "! How are you today?";
+  alert("Hello " + person + "! How are you today?");
 }
 // Signup forms
 // Arithemtic Operators (by adding)
-let x=10;
-let y=2;
-let result=x+y;
-console.log('Result is: '+result)
+let x = 10;
+let y = 2;
+let result = x + y;
+console.log("Result is: " + result);
 // The + sign here is a concatenating operator here because it is used with string
 
-var firstname = 'Alyssa';
-var surname = 'Barrett';
-var fullname = firstname + '' + surname;
+var firstname = "Alyssa";
+var surname = "Barrett";
+var fullname = firstname + "" + surname;
 // the variable fullname now contains the value “Alyssa Barrett”
 
 // comparison variable
-let age= 37
-age==34
+let age = 37;
+age == 34;
 // Want a certain demographic for our buyers of ABA Coin
 
-function checkAge () {
-  const age =document.getElementById("age-input")
-  const ageNum= parseInt(age.value)
-  if (ageNum>49) {
-    alert('Sorry you are not eligible')
-  } else if (ageNum>18&& ageNum<49){
-    alert ('Welcome to the team')
-  }
-   else {
-    alert ('Sorry you are not eligible')
+function checkAge() {
+  const age = document.getElementById("age-input");
+  const ageNum = parseInt(age.value);
+  if (ageNum > 49) {
+    alert("Sorry you are not eligible");
+  } else if (ageNum > 18 && ageNum < 49) {
+    alert("Welcome to the team");
+  } else {
+    alert("Sorry you are not eligible");
   }
 }
 
-
-
 // loops for (let i=0; i<5; o++){}
-for(i = 10; i > 0; i--) {  console.log(i);}
+for (i = 10; i > 0; i--) {
+  console.log(i);
+}
 
 // Main Menu
 
-
-const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.navbar__menu');
-const navLogo = document.querySelector('#navbar__logo');
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".navbar__menu");
+const navLogo = document.querySelector("#navbar__logo");
 
 // Display Mobile Menu
 const mobileMenu = () => {
-  menu.classList.toggle('is-active');
-  menuLinks.classList.toggle('active');
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
 };
 
-menu.addEventListener('click', mobileMenu);
+menu.addEventListener("click", mobileMenu);
 
 // Show active menu when scrolling
 const highlightMenu = () => {
-  const elem = document.querySelector('.highlight');
-  const homeMenu = document.querySelector('#home-page');
-  const aboutMenu = document.querySelector('#about-page');
-  const servicesMenu = document.querySelector('#services-page');
+  const elem = document.querySelector(".highlight");
+  const homeMenu = document.querySelector("#home-page");
+  const aboutMenu = document.querySelector("#about-page");
+  const servicesMenu = document.querySelector("#services-page");
   let scrollPos = window.scrollY;
   // console.log(scrollPos);
 
   // adds 'highlight' class to my menu items
   if (window.innerWidth > 960 && scrollPos < 600) {
-    homeMenu.classList.add('highlight');
-    aboutMenu.classList.remove('highlight');
+    homeMenu.classList.add("highlight");
+    aboutMenu.classList.remove("highlight");
     return;
   } else if (window.innerWidth > 960 && scrollPos < 1400) {
-    aboutMenu.classList.add('highlight');
-    homeMenu.classList.remove('highlight');
-    servicesMenu.classList.remove('highlight');
+    aboutMenu.classList.add("highlight");
+    homeMenu.classList.remove("highlight");
+    servicesMenu.classList.remove("highlight");
     return;
   } else if (window.innerWidth > 960 && scrollPos < 2345) {
-    servicesMenu.classList.add('highlight');
-    aboutMenu.classList.remove('highlight');
+    servicesMenu.classList.add("highlight");
+    aboutMenu.classList.remove("highlight");
     return;
   }
 
   if ((elem && window.innerWIdth < 960 && scrollPos < 600) || elem) {
-    elem.classList.remove('highlight');
+    elem.classList.remove("highlight");
   }
 };
 
-window.addEventListener('scroll', highlightMenu);
-window.addEventListener('click', highlightMenu);
+window.addEventListener("scroll", highlightMenu);
+window.addEventListener("click", highlightMenu);
 
 //  Close mobile Menu when clicking on a menu item
 const hideMobileMenu = () => {
-  const menuBars = document.querySelector('.is-active');
+  const menuBars = document.querySelector(".is-active");
   if (window.innerWidth <= 768 && menuBars) {
-    menu.classList.toggle('is-active');
-    menuLinks.classList.remove('active');
+    menu.classList.toggle("is-active");
+    menuLinks.classList.remove("active");
   }
 };
 
-menuLinks.addEventListener('click', hideMobileMenu);
-navLogo.addEventListener('click', hideMobileMenu);
+menuLinks.addEventListener("click", hideMobileMenu);
+navLogo.addEventListener("click", hideMobileMenu);
 
-
-
-// function checkIfEven(num) {  return (num % 2 === 0) ? true : false;} 
-// function checkIfEven(num) {  if(num % 2 === 0)  
-//  {    return true;  }  
-// else {    return false  }} 
+// function checkIfEven(num) {  return (num % 2 === 0) ? true : false;}
+// function checkIfEven(num) {  if(num % 2 === 0)
+//  {    return true;  }
+// else {    return false  }}
 // console.log("13 is even? " ,checkIfEven(13));
 // console.log("24 is even? " ,checkIfEven(24));
--- week-4/5-friday/practice.js
+++ week-4/5-friday/practice.js
@@ -3,18 +3,59 @@
 // Accessing HTML elements
 
 const slides = document.getElementsByClassName("slider__slide");
-const dots = document.getElementByClassname("slider__dots");
+const dots = document.getElementsByClassName("slider__dots");
+const dots2 = document.querySelectorAll(".slider__dots");
 const prevNavBtn = document.getElementById("nav-button--prev");
 const nextNavBtn = document.getElementById("nav-button--next");
+const caption = document.querySelectorAll(".caption");
+const captionBtn = document.querySelector(".caption-btn");
+const captionToAdd = document.querySelector("input");
+const photoToCaption = document.getElementById("photos");
 
 // Slider Components
 
 let currentSlide = 0;
 
-prevNavBtn.addEventListener("click", function () {
-  changeSlide();
-}); // this is a call back function, a function isn't being used as an argument
+prevNavBtn.addEventListener("click", () => changeSlide(currentSlide - 1));
+// this is a call back function, a function isn't being used as an argument
+nextNavBtn.addEventListener("click", () => changeSlide(currentSlide + 1));
 
-nextNavBtn.addEventListener("click", function () {
-  changeSlide();
+// function changeSlide(moveTo)
+function changeSlide(moveTo) {
+  if (moveTo >= slides.length) {
+    moveTo = 0;
+  }
+  if (moveTo < 0) {
+    moveTo = slides.length - 1;
+  }
+  slides[currentSlide].classList.toggle("active");
+  dots[currentSlide].classList.toggle("active");
+  slides[moveTo].classList.toggle("active");
+  dots[moveTo].classList.toggle("active");
+
+  currentSlide = moveTo;
+}
+
+// Dot functionality
+
+dots2.forEach((dot, index) => {
+  console.log(dot, index);
+  dot.addEventListener("click", () => {
+    if (currentSlide !== index) {
+      changeSlide(index);
+    }
+  });
+});
+
+// user adds a caption functionality
+let captionInputValue;
+captionToAdd.addEventListener("input", (event) => {
+  captionInputValue = event.target.value;
+  console.log(captionInputValue);
+});
+
+captionBtn.addEventListener("click", function () {
+  let photoCaptionToUpdate = photoToCaption.selectedIndex;
+  console.log(photoCaptionToUpdate);
+  caption[photoCaptionToUpdate].textContent = captionInputValue;
 });
