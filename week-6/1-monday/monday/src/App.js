561c965e2e435d31b0c9ad816fc28c71f826b33c
Alyssa Barrett <cicibarrett@gmail.com>
2022-06-17 13:48:58 +1200

    more changes

---
+++ week-6/1-monday/monday/src/App.js
@@ -0,0 +1,11 @@
+import Car from "./Car";
+
+function App() {
+  return (
+    <div>
+      <Car carName="Hellon!!!"></Car>
+    </div>
+  );
+}
+
+export default App;
---
+++ week-6/1-monday/monday/src/Car.js
@@ -0,0 +1,5 @@
+function Car(props) {
+  return <div>The car is a {props.carName}</div>;
+}
+
+export default Car;
